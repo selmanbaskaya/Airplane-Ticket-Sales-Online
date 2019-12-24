80d99720248a8f46c244fc96232c3346d4a70e91
Selman Başkaya <selmanbaskaya1@gmail.com>
2019-12-24 22:13:42 +0300

    Update veritabani.py

--- veritabani.py
+++ veritabani.py
@@ -307,4 +307,4 @@ def rezervasyon_sil():
             return render_template("mesaj.html", kayitlar = msg)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(port=5000, debug=True)
